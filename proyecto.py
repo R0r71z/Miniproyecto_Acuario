ca603ed381a7d6480c2eceee2242097a79dbb0b3
mariogr93 <47310988+mariogr93@users.noreply.github.com>
2019-03-09 12:22:52 -0400

    corregir bugs con nombres y numeros
    creacion de metodos para evaluar si un nombre esta escrito incorrectamente o si un numero tiene letras, tambien se creo un metodo para buscar por pais

--- proyecto.py
+++ proyecto.py
@@ -47,7 +47,7 @@ class Pez():
         self.__cantidad = nuevaCantidad
 
     def set_tipo(self, nuevoTipo):
-        self.__tipo = nuevoTipo #[!!!]
+        self.__tipo = nuevoTipo
 
     def get_nombre(self):
         return self.__nombre
@@ -65,7 +65,7 @@ class Pez():
         return self.__pais
 
     def get_tipo(self):
-        return self.__tipo #[!!!]
+        return self.__tipo
 
     def get_cantidad(self):
         return self.__cantidad
@@ -109,29 +109,32 @@ class Acuario():
 
     def modificar_nombre(self, pez):
         nuevo = raw_input('nuevo nombre ')
-        if int(pez.get_tamano() < 20) or int(pez.get_tamano()) > 80:
-            self.__peces[pez.get_agua()]['GRANDE'].pop(pez.get_nombre())
-        else:
-            self.__peces[pez.get_agua()]['PEQUENO'].pop(pez.get_nombre())
-        pez.set_nombre(nuevo)
-        self.registrar(pez)
+        if self.evaluador_string(nuevo) != False:
+            if int(pez.get_tamano() < 20) or int(pez.get_tamano()) > 80:
+                self.__peces[pez.get_agua()]['GRANDE'].pop(pez.get_nombre())
+            else:
+                self.__peces[pez.get_agua()]['PEQUENO'].pop(pez.get_nombre())
+            pez.set_nombre(nuevo.upper())
+            self.registrar(pez)
 
     def modificar_tamano(self, pez):
         nuevo = raw_input('nuevo tamano ')
-        if int(pez.get_tamano() < 20) or int(pez.get_tamano()) > 80:
-            if int(nuevo) < 20 or int(nuevo) > 80:
-                pez.set_tamano(nuevo)
+        if self.evaluador_integer(nuevo) != False:
+            if int(pez.get_tamano() < 20) or int(pez.get_tamano()) > 80:
+                if int(nuevo) < 20 or int(nuevo) > 80:
+                    pez.set_tamano(nuevo)
+                else:
+                    self.__peces[pez.get_agua()]['GRANDE'].pop(pez.get_nombre())
+                    pez.set_tamano(nuevo)
+                    self.__peces[pez.get_agua()]['PEQUENO'][pez.get_nombre()] = pez
             else:
-                self.__peces[pez.get_agua()]['GRANDE'].pop(pez.get_nombre())
-                pez.set_tamano(nuevo)
-                self.__peces[pez.get_agua()]['PEQUENO'][pez.get_nombre()] = pez
-        else:
-            if int(nuevo) > 80:
-                self.__peces[pez.get_agua()]['PEQUENO'].pop(pez.get_nombre())
-                pez.set_tamano(nuevo)
-                self.__peces[pez.get_agua()]['GRANDE'][pez.get_nombre()] = pez
-            else:
-                pez.set_tamano(nuevo)
+                if int(nuevo) > 80:
+                    self.__peces[pez.get_agua()]['PEQUENO'].pop(pez.get_nombre())
+                    pez.set_tamano(nuevo)
+                    self.__peces[pez.get_agua()]['GRANDE'][pez.get_nombre()] = pez
+                else:
+                    pez.set_tamano(nuevo)
+
 
     def modificar_pais(self, pez):
         nuevo = raw_input('Nuevo pais ')
@@ -141,32 +144,57 @@ class Acuario():
         print 'NOTA: Recuerde que el limite de animales por especie es: \nAnimales mayor a 20CM = 8 \nAnimales mayor a 80CM = 3'
         print 'Cantidad actual ' + pez.get_cantidad()
         nuevo = raw_input('Nueva cantidad ')
-        pez.set_cantidad(nuevo)
-        print 'Cantidad Modificada'
-
-    def buscar_nombre(self):
-        nombre = raw_input('Nombre del Animal a buscar ')
-        animal = 'Animal no encontrado'
-        for agua in self.get_peces():
-            for tanque in self.get_peces()[agua]:
-                if nombre.upper() in self.get_peces()[agua][tanque]:
-                    animal = self.get_peces()[agua][tanque][nombre.upper()]
-        return animal
+        if self.evaluador_integer(nuevo) != False:
+            pez.set_cantidad(nuevo)
+            print 'Cantidad Modificada'
 
     def mostrar(self):
         nombre = self.buscar_nombre()
-        lista = {'A': nombre.get_nombre(), 'B': nombre.get_agua(), 'C': nombre.get_tamano(),
-                 'D': nombre.get_temperatura(), 'E': nombre.get_pais(), 'F': nombre.get_tipo(),
-                 'G': nombre.get_cantidad(), 'H': ['Nombre: ' + nombre.get_nombre(),
-                                                   'Tipo de animal: ' + nombre.get_tipo(),
-                                                   'De agua: ' + nombre.get_agua(),
-                                                   'Tamano aproximado de: ' + nombre.get_tamano(),
-                                                   'Temperatura recomendable: '
-                                                   + nombre.get_temperatura(), 'Pais nativo: ' + nombre.get_pais(),
-                                                   'Cantidad en tanque: ' + nombre.get_cantidad()]}
-        print 'a)Nombre\nb)Agua\nc)Tamano\nd)Temperatura\ne)Pais\nf)Tipo\ng)Cantidad\nh)TODO'
-        ver = raw_input('que desea ver? (a,b,c,d,e,f,g,h) ')
-        return lista[ver.upper()]
+        retornar = 'Nombre Incorrecto'
+        if nombre != False:
+            lista = {'A': nombre.get_nombre(), 'B': nombre.get_agua(), 'C': nombre.get_tamano(),
+                     'D': nombre.get_temperatura(), 'E': nombre.get_pais(), 'F': nombre.get_tipo(),
+                     'G': nombre.get_cantidad(), 'H': ['Nombre: ' + nombre.get_nombre(),
+                                                       'Tipo de animal: ' + nombre.get_tipo(),
+                                                       'De agua: ' + nombre.get_agua(),
+                                                       'Tamano aproximado de: ' + nombre.get_tamano(),
+                                                       'Temperatura recomendable: '
+                                                       + nombre.get_temperatura(), 'Pais nativo: ' + nombre.get_pais(),
+                                                       'Cantidad en tanque: ' + nombre.get_cantidad()]}
+            print 'a)Nombre\nb)Agua\nc)Tamano\nd)Temperatura\ne)Pais\nf)Tipo\ng)Cantidad\nh)TODO'
+            ver = raw_input('que desea ver? (a,b,c,d,e,f,g,h) ')
+            retornar = lista[ver.upper()]
+
+
+        return retornar
+
+    def mostrar_por_pais(self):
+        pais = raw_input('Nombre del pais ')
+        animales = []
+        for agua in self.get_peces():
+            for tanque in self.get_peces()[agua]:
+                for animal in self.get_peces()[agua][tanque]:
+                    if pais.upper() == self.get_peces()[agua][tanque][animal].get_pais():
+                        animales.append(self.get_peces()[agua][tanque][animal])
+
+        for nombre in animales:
+            print ['Nombre: ' + nombre.get_nombre(),'Tipo de animal: ' + nombre.get_tipo(),
+             'De agua: ' + nombre.get_agua(),'Tamano aproximado de: ' + nombre.get_tamano(),
+              'Temperatura recomendable: ' + nombre.get_temperatura(), 'Pais nativo: ' + nombre.get_pais(),
+              'Cantidad en tanque: ' + nombre.get_cantidad()]
+
+
+
+
+    def buscar_nombre(self):
+        nombre = raw_input('Nombre del Animal a buscar ')
+        animal = False
+        if self.evaluador_string(nombre) != False:
+            for agua in self.get_peces():
+                for tanque in self.get_peces()[agua]:
+                    if nombre.upper() in self.get_peces()[agua][tanque]:
+                        animal = self.get_peces()[agua][tanque][nombre.upper()]
+        return animal
 
     def eliminar(self):
         nombre = raw_input('Escriba el nombre del animal que desea eliminar del registro ')
@@ -176,6 +204,21 @@ class Acuario():
                     self.get_peces()[agua][tanque].pop(nombre.upper())
                     return 'Eliminacion Completada'
         return 'No encontrado'
+
+
+    def evaluador_integer(self,integer):
+        numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        for element in integer:
+            if element not in numeros:
+                return false
+
+
+    def evaluador_string(self, string):
+        letras = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+        for element in string.upper():
+            if element not in letras:
+                return False
 
     def get_peces(self):
         return self.__peces
@@ -191,7 +234,11 @@ print acu.registrar(peje2)
 print acu.registrar(tiburon)
 print acu.get_peces()
 print '------------------------------------'
+#print acu.modificar_nombre(peje1)
+print acu.mostrar_por_pais()
+print '------------------------------------'
+print acu.get_peces()
 # print peje1.get_tamano()
 # acu.modificar_tamano(peje1)
 # print '------------------------------------'
-print acu.mostrar()
+
